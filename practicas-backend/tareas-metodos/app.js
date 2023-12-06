1a62b80cbdab8925194ed457e4a830c6c3a91972
Marina Mansueto <mansumdp@gmail.com>
2023-12-06 18:22:52 -0300

    partials

--- practicas-backend/tareas-metodos/app.js
+++ practicas-backend/tareas-metodos/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts');
+
 const PORT = 3000;
 
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
+
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
+
 
 app.use(require('./src/routes/tareasRoutes'));
 
