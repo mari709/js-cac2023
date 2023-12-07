9a80722b84eabba85821cac41ee8650d6c8046be
Marina Mansueto <mansumdp@gmail.com>
2023-12-06 22:11:48 -0300

    comentarios

--- practicas-backend/tareas-metodos/src/controllers/tareasController.js
+++ practicas-backend/tareas-metodos/src/controllers/tareasController.js
@@ -1,15 +1,15 @@
-let tareas = [
+const tareas = [
     { id: 1, title: "Tarea 1", completed: false },
     { id: 2, title: "Tarea 2", completed: false },
     { id: 3, title: "Tarea 3", completed: false },
     { id: 4, title: "Tarea 4", completed: false },
   ];
   
-  
+  // Renderiza la página de índice con las tareas
   const index = (req, res) => {
       res.render("index", {tareas});
   };
-
+// Agrega una nueva tarea al array de tareas
   const store = (req, res) => {
     const tarea = {
       id: Date.now(),
@@ -20,28 +20,25 @@ let tareas = [
   
     res.redirect("/");
   };
-  
+
+// Cambia el estado de completitud de una tarea
 const update = (req, res) => {
     tareas.forEach((tarea) => {
-    if (tarea.id == req.params.id) {
+    if (tarea.id === req.params.id) {
         tarea.completed = !tarea.completed;
     }
     });
 
     res.redirect("/");
 };
+
+// Elimina una tarea del array de tareas
 const destroy = (req, res) => {
     tareas = tareas.filter((tarea) => tarea.id != req.params.id);
 
     res.redirect("/");
 };
 
-/*
-router.get('/', controller.index);
-router.post('/', controller.store); //parte de administracion
-router.put('/:id', controller.update); //parte de actualizacion 
-router.delete('/:id', controller.destroy); //parte de de eliminar tachar
-*/
   module.exports ={
       index,
       store,
